efd0d3ac1804c4fc22aaa2592a3aa117e36ad4e9
Praja04 <rajadamang13@gmail.com>
2024-08-01 13:33:25 +0700

    memperbaiki tampilan uploader

--- app/Views/user/table_number.php
+++ app/Views/user/table_number.php
@@ -164,26 +164,6 @@
             "ordering": true,
             "info": true,
             "autoWidth": false,
-            "initComplete": function() {
-                this.api().columns().every(function() {
-                    var column = this;
-                    var select = $('<select class="form-control"><option value=""></option></select>')
-                        .appendTo($(column.header()).empty())
-                        .on('change', function() {
-                            var val = $.fn.dataTable.util.escapeRegex(
-                                $(this).val()
-                            );
-
-                            column
-                                .search(val ? '^' + val + '$' : '', true, false)
-                                .draw();
-                        });
-
-                    column.data().unique().sort().each(function(d, j) {
-                        select.append('<option value="' + d + '">' + d + '</option>');
-                    });
-                });
-            }
         });
 
         $('.btn-pdf-modal').on('click', function() {
