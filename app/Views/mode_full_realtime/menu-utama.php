fa193c3ee70b9d6e490369cc6cc3dbffa643e6d8
Praja04 <rajadamang13@gmail.com>
2025-01-07 13:43:48 +0700

    memperbarui tampilan menu

---
+++ app/Views/mode_full_realtime/menu-utama.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PT. Century Batteries Indonesia</title>
+
+    <!-- Icons -->
+    <!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
+    <link rel="shortcut icon" href="<?= base_url() . 'assets/images/favicons/favicon.png' ?>">
+    <link rel="icon" type="image/png" sizes="192x192" href="<?= base_url() . 'assets/images/favicons/favicon-192x192.png' ?>">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url() . 'assets/images/favicons/apple-touch-icon-180x180.png' ?>">
+    <!-- END Icons -->
+    <link rel="stylesheet" href="<?= base_url() ?>assets/menu-utama.css">
+
+</head>
+
+<body>
+    <div class="context">
+        <h1>Digitalisasi Parameter Proses Battery</h1><br>
+        <div class="container">
+            <a href="<?= base_url('produksi1') ?>">
+                <div class="card">
+                    <div class="slide slide1">
+                        <div class="content">
+                            <div class="icon">
+                                <img style="width: 100px;" class="img-fluid" src="<?= base_url('assets/images/menu/manufacture.png') ?>" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="slide slide2">
+                        <div class="content">
+                            <h3>
+                                Produksi 1
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+            </a>
+            <a href="<?= base_url('produksi2') ?>">
+                <div class="card">
+                    <div class="slide slide1">
+                        <div class="content">
+                            <div class="icon">
+                                <img style="width: 100px;" class="img-fluid" src="<?= base_url('assets/images/menu/product.png') ?>" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="slide slide2">
+                        <div class="content">
+                            <h3>
+                                Produksi 2
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+            </a>
+            <a href="<?= base_url('charging') ?>">
+                <div class="card">
+                    <div class="slide slide1">
+                        <div class="content">
+                            <div class="icon">
+                                <img style="width: 100px;" class="img-fluid" src="<?= base_url('assets/images/menu/hemodialysis-machine.png') ?>" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="slide slide2">
+                        <div class="content">
+                            <h3>
+                                Charging & Industrial Battery
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+
+    <div class="area">
+        <ul class="circles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+</body>
+
+</html>
